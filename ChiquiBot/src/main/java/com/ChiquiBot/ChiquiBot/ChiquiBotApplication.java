b103aa7d6e1bb63eddffa985e1f70bd6116a0dde
FlaviaSalazar27 <flaviasalazarovalle@gmail.com>
2019-11-14 17:29:17 -0400

    Inicio

---
+++ ChiquiBot/src/main/java/com/ChiquiBot/ChiquiBot/ChiquiBotApplication.java
@@ -0,0 +1,13 @@
+package com.ChiquiBot.ChiquiBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiquiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiquiBotApplication.class, args);
+	}
+
+}
