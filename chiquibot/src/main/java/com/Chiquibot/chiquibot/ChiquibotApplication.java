79bc6e76bbee573a2101eb145de7bee89812fb81
FlaviaSalazar27 <flaviasalazarovalle@gmail.com>
2019-11-22 19:11:38 -0400

    BASE DE DATOS

---
+++ chiquibot/src/main/java/com/Chiquibot/chiquibot/ChiquibotApplication.java
@@ -0,0 +1,13 @@
+package com.Chiquibot.chiquibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiquibotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiquibotApplication.class, args);
+	}
+
+}
---
+++ chiquibot/src/test/java/com/Chiquibot/chiquibot/ChiquibotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Chiquibot.chiquibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiquibotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
